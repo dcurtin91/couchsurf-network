f057c38f4a8f6e50f408d5b354e0b8429733a742
dave <dcurtin91@gmail.com>
2023-07-08 16:16:15 -0500

    f

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path'; // Add this line
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/member-portal/",
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, 'public/404.html'),
+      },
+    },
+  },
+});
