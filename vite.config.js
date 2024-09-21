6e5e9896b9be6f3384352aae0da0fa68695b4165
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-09-21 17:07:27 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path'; // Add this line
+import { resolve } from 'path'; 
 
 export default defineConfig({
   plugins: [react()],
